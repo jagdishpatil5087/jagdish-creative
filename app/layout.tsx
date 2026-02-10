a281655c62d1fc24f3a9cb3f51acfbfeedec99c5
Jagdish Patil <jagdishpatil5087@gmail.com>
2026-02-10 23:25:35 +0530

    fix nextjs analytics issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Script from "next/script";
-import Analytics from "./view-data/analytics";
+import Analytics from "./analytics";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
